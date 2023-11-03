3f28d8f2f7bb304f3e564e58954cea55a9d8e139
Johan Hennius <johan.bh19@gmail.com>
2023-11-03 16:11:24 +0000

    Add function to update stock

--- run.py
+++ run.py
@@ -95,6 +95,22 @@ def get_last_five_sales():
 
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate average stock for each sandwich
+    """
+    print("Calculating stock data...")
+
+    new_stock_data = []
+    
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
 
 def main():
     """
@@ -105,8 +121,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_Data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_five_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to the future, sandwich eater.\n")
-# main()
+main()
 
-sales_columns = get_last_five_sales()
